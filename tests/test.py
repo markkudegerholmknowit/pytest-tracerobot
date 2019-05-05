7b864ab2cfc68e644c9bf02483b63636a2f66440
Markku Degerholm <markku@degerholm.fi>
2019-05-05 21:43:20 +0300

    WIP

--- tests/test.py
+++ tests/test.py
@@ -52,38 +52,46 @@ def testFixture2():
 
 @pytest.mark.critical
 def test_passing_test():
-    """ Does this get logged? """
+    """ A simple passing test """
     check_sum(1,2,3)
 
 @pytest.mark.critical
 def test_direct_assert():
+    """ A test that fails with inlined assertion error """
     rlog("here!")
     raise AssertionError("foo")
 
 @pytest.mark.critical
 def test_keyword_assert():
+    """ A test that fails within keyword """
     check_sum(1,2,4)
 
 @pytest.mark.critical
 def test_fixture_setup(fixtureWithSetup):
+    """ A test with a fixture with a setup """
     rlog("here")
 
 @pytest.mark.critical
 def test_fixture_setup_and_teardown(fixtureWithSetupAndTeardown1):
+    """ A test with passing fixture setup and teardown """
     rlog("here")
 
 @pytest.mark.critical
 def test_two_fixtures(fixtureWithSetupAndTeardown1, fixtureWithSetupAndTeardown2):
+    """ A test with two function-scoped fixtures """
     rlog("here")
 
 @pytest.mark.critical
 def test_module_and_test_fixtures(moduleFixture, fixtureWithSetupAndTeardown1):
+    """ A test with one module-scoped and one function-scoped fixture """
     rlog("here")
 
 @pytest.mark.critical
 def test_setup_assert(fixtureWithSetupError):
+    """ A test that fails in fixture setup phase """
     rlog("here")
 
 @pytest.mark.critical
 def test_teardown_assert(fixtureWithTeardownError):
+    """ A test that fails in fixture teardown phase """
     rlog("here")
